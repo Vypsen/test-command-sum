f93bbd3b2c6ac0fb649a3b34f26e5b5fb9bd319e
Vypsen <s.semenets2001@mail.ru>
2022-12-21 16:21:52 +1000

    fix unknown and add color

--- Commands/GetSumCountAllFilesCommand.php
+++ Commands/GetSumCountAllFilesCommand.php
@@ -2,6 +2,7 @@
 
 namespace Console\Commands;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -15,16 +16,28 @@ class GetSumCountAllFilesCommand extends Command
     {
         $this->setName("app:get-sum")
             ->setDescription('Get all sum count all nested directories')
-            ->addArgument('dir', InputArgument::REQUIRED, 'directory ');
+            ->addArgument('dir', InputArgument::REQUIRED, 'directory');
 
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $dir = $input->getArgument('dir');
-        $this->getReqSumCount($dir);
-        $output->writeln("сумма чисел в файлах count = {$this->sumAll}");
-        return Command::SUCCESS;
+        if (file_exists($dir)) {
+            $this->getReqSumCount($dir);
+
+            $outputStyle = new OutputFormatterStyle('green', 'gray', ['bold', 'blink']);
+            $output->getFormatter()->setStyle('fire', $outputStyle);
+
+            $output->writeln("<fire>сумма чисел в файлах count = {$this->sumAll}</>");
+            return Command::SUCCESS;
+        } else {
+            $outputStyle = new OutputFormatterStyle('red', 'gray', ['bold', 'blink']);
+            $output->getFormatter()->setStyle('fire', $outputStyle);
+
+            $output->writeln('<fire>unknown directory</>');
+            return Command::INVALID;
+        }
     }
 
     private function getReqSumCount($dir)
@@ -33,7 +46,7 @@ class GetSumCountAllFilesCommand extends Command
         foreach ($files as $file) {
             $path = $dir. '/'. $file;
             if (is_dir($path)) {
-                return $this->getReqSumCount($path);
+                $this->sumAll += $this->getReqSumCount($path);
             } else {
                 $countFile = file($path);
                 if (!empty($countFile)) {
